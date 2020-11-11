f9d8f8c49429e385ba906e4188f8549e08e01fa1
guvarallo <gustavo.varallo@live.com>
2020-11-11 16:46:11 -0300

    updated styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
@@ -16,13 +16,16 @@ function App() {
   const [error, setError] = useState('');
   const [result, setResult] = useState({});
 
-  navigator.geolocation.getCurrentPosition(pos => {
-    // Get only 2 decimals for lat and lon, since the longer lon was giving error
-    setLat(Math.round(pos.coords.latitude * 100) / 100);
-    setLon(Math.round(pos.coords.longitude * 100) / 100);
-  });
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(pos => {
+      // Get only 2 decimals for lat and lon, since the longer lon was giving error
+      setLat(Math.round(pos.coords.latitude * 100) / 100);
+      setLon(Math.round(pos.coords.longitude * 100) / 100);
+      handleWeatherFetch();
+    });
+  }, []);
 
-  function handleClick() {
+  function handleWeatherFetch() {
     const currentTime = new Date();
     setTime(`${currentTime.getHours()}:${currentTime.getMinutes()}`);
 
@@ -30,7 +33,13 @@ function App() {
     Weather.getWeatherFromUserLocation(lat, lon)
       .then(data => {
         console.log(data);
-        setResult(data);
+        setResult({
+          temp: Math.round(data.main.temp),
+          feels_like: Math.round(data.main.feels_like),
+          name: data.name,
+          icon: data.weather[0].icon,
+          description: data.weather[0].description,
+        });
         setLoading(false);
       })
       .catch(err => {
@@ -44,15 +53,12 @@ function App() {
 
   return (
     <Container maxWidth="lg" style={{ textAlign: 'center' }}>
-      <div>
+      <div className="main">
         <h1>Welcome to the Weather App!</h1>
         <h4>
           Please click the button for updated weather information on your
           current location:
         </h4>
-        <Button variant="contained" color="primary" onClick={handleClick}>
-          Click
-        </Button>
       </div>
       {loading && (
         <>
@@ -60,20 +66,31 @@ function App() {
           <CircularProgress />
         </>
       )}
-      {result.name && (
-        <Paper elevation={3} style={{ marginTop: '20px' }}>
-          <div id="city">
+      {result.name && !loading && (
+        <Paper className="paper" elevation={3} style={{ marginTop: '20px' }}>
+          <div className="city">
             <h1>{result.name}</h1>
             <p>Local time: {time}</p>
           </div>
-          <div className="weather-icon">
-            <img
-              src={`http://openweathermap.org/img/w/${result.weather[0].icon}.png`}
-              alt="weather icon"
-            />
+          <div>
+            <div className="weather-icon">
+              <img
+                src={`http://openweathermap.org/img/w/${result.icon}.png`}
+                alt="weather icon"
+              />
+              <h3>{result.temp}°C</h3>
+            </div>
+            <p>
+              Feels like: {result.feels_like}°C, {result.description}.
+            </p>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleWeatherFetch}
+            >
+              Update Info
+            </Button>
           </div>
-          <h3>{result.main.temp}</h3>
-          <p>{result.weather[0].description}</p>
         </Paper>
       )}
       {error && <p>{error}</p>}
