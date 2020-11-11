a8c135de53533466e841467dc149e86b955a62e2
guvarallo <gustavo.varallo@live.com>
2020-11-11 12:26:28 -0300

    tested api fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,27 @@
+import React, { useEffect, useState } from 'react';
+
 function App() {
+  const [lat, setLat] = useState('');
+  const [lon, setLon] = useState('');
+  const apiKey = '7ac325fd18f5ce43cc1cc62f3e3da84f';
+  const [result, setResult] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(pos => {
+      setLat(pos.coords.latitude);
+      setLon(pos.coords.longitude);
+    });
+  }, []);
+
+  if (lat && lon) {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&unit=metric`,
+    )
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  }
+
   return <h1>Hey!</h1>;
 }
 
