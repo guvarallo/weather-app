94b2b72f3c968e423dfe8c432f2ed87ff038a8f4
guvarallo <gustavo.varallo@live.com>
2020-11-11 14:56:23 -0300

    added material ui

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,18 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState } from 'react';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
+import './App.css';
 import Weather from '../../utils/WeatherAPI';
 
 function App() {
   const [lat, setLat] = useState(0);
   const [lon, setLon] = useState(0);
+  const [time, setTime] = useState('');
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
   const [result, setResult] = useState({});
 
@@ -16,29 +23,55 @@ function App() {
   });
 
   function handleClick() {
+    const currentTime = new Date();
+    setTime(`${currentTime.getHours()}:${currentTime.getMinutes()}`);
+
+    setLoading(true);
     Weather.getWeatherFromUserLocation(lat, lon)
-      .then(data => setResult(data))
+      .then(data => {
+        console.log(data);
+        setResult(data);
+        setLoading(false);
+      })
       .catch(err => {
         setError(
           'Something went wrong, please try again or contact us for further investigation.',
         );
+        setLoading(false);
         console.log(err);
       });
   }
 
   return (
-    <>
-      <button onClick={handleClick}>Click</button>
-      {result.name && (
+    <Container maxWidth="lg" style={{ textAlign: 'center' }}>
+      <div>
+        <h1>Welcome to the Weather App!</h1>
+        <h4>
+          Please click the button for updated weather information on your
+          current location:
+        </h4>
+        <Button variant="contained" color="primary" onClick={handleClick}>
+          Click
+        </Button>
+      </div>
+      {loading && (
         <>
-          <h1>{result.name}</h1>
-          <h3>{result.main.temp}</h3>
-          <p>{result.weather[0].description}</p>
+          <h4>Please wait while we get the information</h4>
+          <CircularProgress />
         </>
       )}
-      {!result.name && !error && <h1>Loading...</h1>}
+      {result.name && (
+        <Paper elevation={3} style={{ marginTop: '20px' }}>
+          <div id="city">
+            <h1>{result.name}</h1>
+            <p>Local time: {time}</p>
+          </div>
+          <h3>{result.main.temp}</h3>
+          <p>{result.weather[0].description}</p>
+        </Paper>
+      )}
       {error && <p>{error}</p>}
-    </>
+    </Container>
   );
 }
 
