d538f15fbb351b77c2bdc901673d864fdea54b07
guvarallo <gustavo.varallo@live.com>
2020-11-12 09:26:51 -0300

    code refactoring

--- src/actions.js
+++ src/actions.js
@@ -4,7 +4,7 @@ const url = 'http://api.openweathermap.org/data/2.5/weather?';
 const key = apiConfig.apiKey;
 
 export const fetchWeather = () => {
-  return async (dispatch, getState) => {
+  return async dispatch => {
     dispatch({ type: 'FETCH_WEATHER_PENDING' });
     let lat, lon;
     const getPosition = () => {
@@ -28,26 +28,3 @@ export const fetchWeather = () => {
     });
   };
 };
-
-// export const fetchWeather = async () => {
-//   let lat, lon;
-//   const getPosition = () => {
-//     return new Promise(function (resolve, reject) {
-//       navigator.geolocation.getCurrentPosition(resolve, reject);
-//     });
-//   };
-
-//   const data = await getPosition().then(pos => {
-//     // Get only 2 decimals for lat and lon, since the longer lon was giving error
-//     lat = Math.round(pos.coords.latitude * 100) / 100;
-//     lon = Math.round(pos.coords.longitude * 100) / 100;
-//     return fetch(`${url}lat=${lat}&lon=${lon}&appid=${key}&units=metric`)
-//       .then(res => res.json())
-//       .then(data => data);
-//   });
-
-//   return {
-//     type: 'FETCH_WEATHER',
-//     payload: data,
-//   };
-// };
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,76 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 import Container from '@material-ui/core/Container';
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
-// import Alert from '@material-ui/lab/Alert';
-import CircularProgress from '@material-ui/core/CircularProgress';
 
 import './App.css';
-import { fetchWeather } from '../../actions';
+import Weather from '../Weather/Weather';
 
 function App() {
-  const [time, setTime] = useState('');
-  const dispatch = useDispatch();
-  const { isPending, weather } = useSelector(state => state);
-
-  function handleUpdateInfo() {
-    dispatch(fetchWeather());
-  }
-
-  useEffect(() => {
-    const currentTime = new Date();
-    const hours = currentTime.getHours();
-    const minutes = currentTime.getMinutes();
-    setTime(`${hours}:${minutes < 10 ? `0${hours}` : minutes}`);
-    dispatch(fetchWeather());
-  }, [dispatch]);
-
   return (
     <Container maxWidth="lg" style={{ textAlign: 'center' }}>
       <div className="main">
         <h1>Welcome to the Weather App!</h1>
         <h4>Forecast in real time for your location.</h4>
       </div>
-      {isPending && (
-        <>
-          <h4>Please wait while we get the information</h4>
-          <CircularProgress color="secondary" />
-        </>
-      )}
-      {weather.name && !isPending && (
-        <Paper className="paper" elevation={3} style={{ marginTop: '20px' }}>
-          <div className="city">
-            <h1>{weather.name}</h1>
-            <p>Local time: {time}</p>
-          </div>
-          <div>
-            <div className="weather-icon">
-              <img
-                src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
-                alt="weather icon"
-              />
-              <h3>
-                {Math.round(weather.main.temp)}°C,{' '}
-                {weather.weather[0].description}.
-              </h3>
-            </div>
-            <p>Feels like: {Math.round(weather.main.feels_like)}°C.</p>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleUpdateInfo}
-            >
-              Update Info
-            </Button>
-          </div>
-        </Paper>
-      )}
-      {/* {error && (
-        <Alert severity="error" style={{ width: '70%', margin: 'auto' }}>
-          {error}
-        </Alert>
-      )} */}
+      <Weather />
     </Container>
   );
 }
