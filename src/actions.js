abac18490c9d39884d2b102945c3937a04b4affb
guvarallo <gustavo.varallo@live.com>
2020-11-12 10:32:58 -0300

    added error handling when user denies geolocation

--- src/actions.js
+++ src/actions.js
@@ -13,15 +13,17 @@ export const fetchWeather = () => {
       });
     };
 
-    const data = await getPosition().then(pos => {
-      // Get only 2 decimals for lat and lon, since the longer lon was giving error
-      lat = Math.round(pos.coords.latitude * 100) / 100;
-      lon = Math.round(pos.coords.longitude * 100) / 100;
-      return fetch(`${url}lat=${lat}&lon=${lon}&appid=${key}&units=metric`)
-        .then(res => res.json())
-        .then(data => data)
-        .catch(err => err);
-    });
+    const data = await getPosition()
+      .then(pos => {
+        // Get only 2 decimals for lat and lon, since the longer lon was giving error
+        lat = Math.round(pos.coords.latitude * 100) / 100;
+        lon = Math.round(pos.coords.longitude * 100) / 100;
+        return fetch(`${url}lat=${lat}&lon=${lon}&appid=${key}&units=metric`)
+          .then(res => res.json())
+          .then(data => data)
+          .catch(err => err);
+      })
+      .catch(err => err);
 
     if (data.message) {
       dispatch({ type: 'FETCH_WEATHER_ERROR', payload: data.message });
